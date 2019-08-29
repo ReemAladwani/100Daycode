52ec8bb9b6a3b4adf9ddc80bda9bfabb09b4efcf
user <user@MacBook-Pro-alkhas-b-user.local>
2019-08-30 01:19:06 +0300

    week 1

---
+++ Week1/Day1.py
@@ -0,0 +1,17 @@
+Python 3.8.0b3 (v3.8.0b3:4336222407, Jul 29 2019, 09:46:03) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello, world!)
+      
+SyntaxError: EOL while scanning string literal
+>>> print("Hello, world!")
+Hello, world!
+>>> exit()
+>>> if 5>2
+SyntaxError: invalid syntax
+>>> if 5>2 :
+	print("Five is greater than two!")
+
+	
+Five is greater than two!
+>>> 
---
+++ Week1/Day2.py
@@ -0,0 +1,26 @@
+Python 3.8.0b3 (v3.8.0b3:4336222407, Jul 29 2019, 09:46:03) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #This is a cooment
+>>> #This is a comment
+>>> print("Hello,world!")
+Hello,world!
+>>> print("Hello, world!") #This is a comment
+Hello, world!
+>>> #print("Hello, world!")
+>>> print("cheers,Mete!")
+cheers,Mete!
+>>> #This is a comment
+>>> #written in
+>>> #more than just one line
+>>> print("Hello, world!")
+Hello, world!
+>>> """
+This is a comment
+written in
+more than one line
+"""
+'\nThis is a comment\nwritten in\nmore than one line\n'
+>>> print("Hello, world")
+Hello, world
+>>> 
